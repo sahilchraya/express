0c60f99ec0ea77e272884faa502216a5c09e1784
Sahil Chraya <sahil@crankupit.com>
2022-10-27 10:42:03 +0530

    views
    404

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(shopRoutes);
 
 // 404 Page Route
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 // Create server using express
